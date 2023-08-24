e04395340c9d78c199020d185dd1ecac836c4d86
Alex Bukata <a.bukata@rarible.com>
2023-08-24 13:15:53 +0100

    publish conf

--- protobuf/build.gradle.kts
+++ protobuf/build.gradle.kts
@@ -1,16 +1,21 @@
-import com.google.protobuf.gradle.*
-
+import com.google.protobuf.gradle.generateProtoTasks
+import com.google.protobuf.gradle.id
+import com.google.protobuf.gradle.plugins
+import com.google.protobuf.gradle.proto
+import com.google.protobuf.gradle.protobuf
+import com.google.protobuf.gradle.protoc
 
 plugins {
     id("com.google.protobuf") version "0.8.15"
     `java-library`
+    maven
     `maven-publish`
     signing
     id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
 }
 
-val protobufVersion = "3.14.0"
-val grpcVersion = "1.35.0"
+val protobufVersion = "3.24.1"
+val grpcVersion = "1.57.2"
 
 protobuf {
     protoc {
@@ -36,12 +41,6 @@ dependencies {
     api("io.grpc:grpc-protobuf:$grpcVersion")
     api("io.grpc:grpc-stub:$grpcVersion")
     api("javax.annotation:javax.annotation-api:1.3.2")
-}
-
-nexusPublishing {
-    repositories {
-        sonatype()
-    }
 }
 
 publishing {
@@ -73,12 +72,24 @@ publishing {
             }
         }
     }
+    repositories {
+        maven {
+            credentials.username = System.getenv("NEXUS_USERNAME")
+            credentials.password = System.getenv("NEXUS_PASSWORD")
+
+            if (project.version.toString().endsWith("-SNAPSHOT")) {
+                url = uri("http://nexus.rarible.int/repository/maven-snapshots/")
+            } else {
+                url = uri("http://nexus.rarible.int/repository/maven-releases/")
+            }
+        }
+    }
 }
 
-signing {
-    useGpgCmd() //use gpg2
-    sign(publishing.publications["mavenJava"])
-}
+//signing {
+//    useGpgCmd() //use gpg2
+//    sign(publishing.publications["mavenJava"])
+//}
 
 sourceSets {
     main {
@@ -104,9 +115,10 @@ tasks.compileJava {
 
 repositories {
     mavenCentral()
+    mavenLocal()
 }
 
 group = "org.onflow"
 
 // TODO - grab version from Git
-version = "0.21"
+version = "0.22.rarible"
